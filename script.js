c9b66d9993fd417ce14c6e3c22ed988cc42f567f
chetan <chetanishi986@gmail.com>
2025-10-06 18:55:29 +0530

    third commit

--- script.js
+++ script.js
@@ -228,6 +228,6 @@
         }
         function toTitleCase(str) { return str.replace(/\w\S*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()); }
 
-        fetchWeatherData('Surat');
+        fetchWeatherData('Shirpur');
         
     });
